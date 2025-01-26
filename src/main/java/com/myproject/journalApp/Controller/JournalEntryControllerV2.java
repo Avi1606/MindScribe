1817a2d2423e1f482e931233eea123dd156d9791
Avi1606 <aviiiprajapati16@gmail.com>
2025-01-26 18:03:07 +0530

    Initial commit

---
+++ src/main/java/com/myproject/journalApp/Controller/JournalEntryControllerV2.java
@@ -0,0 +1,51 @@
+package com.myproject.journalApp.Controller;
+
+import com.fasterxml.jackson.databind.ser.impl.ObjectIdWriter;
+import com.myproject.journalApp.Services.JournalEntryServices;
+import com.myproject.journalApp.entity.JournalEntry;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/journal")
+public class JournalEntryControllerV2 {
+
+    @Autowired
+    private JournalEntryServices ServiceEntry; // it is null without adding @Component in that class
+
+    @PostMapping
+    public JournalEntry CreateEntry(@RequestBody JournalEntry MyEntry) {
+        MyEntry.setDate(LocalDateTime.now());
+        ServiceEntry.SaveEntry(MyEntry);
+        return MyEntry;
+    }
+
+    @GetMapping
+    public List<JournalEntry> getall() {
+        return ServiceEntry.getall();
+    }
+
+    @GetMapping("id/{MyId}")
+    public JournalEntry findbyid(@PathVariable ObjectId MyId) {
+        return ServiceEntry.FindById(MyId).orElse(null);
+    }
+
+    @DeleteMapping("id/{MyId}")
+    public boolean deletebyid(@PathVariable ObjectId MyId) {
+        ServiceEntry.DeleteById(MyId);
+        return true;
+    }
+
+    @DeleteMapping
+    public void deletall() {
+        ServiceEntry.DeleteAll();
+    }
+
+
+}
