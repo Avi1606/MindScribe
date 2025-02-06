4ba18f435fb4ae9197c9ae9eff1f48bc9be5112b
Avikumar <aviiiprajapati16@gmail.com>
2025-02-06 14:32:10 +0530

    Initial commit

--- src/main/java/com/myproject/journalApp/Repository/JournalRepository.java
+++ src/main/java/com/myproject/journalApp/Repository/JournalRepository.java
@@ -5,4 +5,4 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface JournalRepository extends MongoRepository<JournalEntry , ObjectId> {
-}
+     }
