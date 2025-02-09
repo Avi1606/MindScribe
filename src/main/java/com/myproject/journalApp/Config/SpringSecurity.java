0d930cf5a64657376b6efb60b7bded2158117999
Avikumar <aviiiprajapati16@gmail.com>
2025-02-10 00:11:12 +0530

    Initial commit

--- src/main/java/com/myproject/journalApp/Config/SpringSecurity.java
+++ src/main/java/com/myproject/journalApp/Config/SpringSecurity.java
@@ -16,6 +16,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 
