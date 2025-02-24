f8e05b763b926fd8bd17067b6bfa21fffa303c4e
Avikumar <aviiiprajapati16@gmail.com>
2025-02-24 06:04:59 +0530

    mail sender added with test case

---
+++ src/main/java/com/myproject/journalApp/Services/EmailService.java
@@ -0,0 +1,4 @@
+package com.myproject.journalApp.Services;
+
+public class EmailService {
+}
---
+++ src/test/java/com/myproject/journalApp/EmailServiceTest/EmailServiceTest.java
@@ -0,0 +1,4 @@
+package com.myproject.journalApp.EmailServiceTest;
+
+public class EmailServiceTest {
+}
